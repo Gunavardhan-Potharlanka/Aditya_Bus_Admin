6805671db011667d134e0ce14942a8913925914b
VeeraMahendra7 <Veeramahindra787@gmail.com>
2024-03-19 17:21:24 +0530

    added bavigation

--- src/components/LoginPage/Navbar.jsx
+++ src/components/LoginPage/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <header className='h-[70px] w-full md:bg-[#fd7520e4] px-5'>
         <nav className='h-full w-full flex items-center md:justify-start justify-center gap-x-[400px]'>
         <div className='h-full w-[90px] p-2'>
-            <img src="favicon.png" className='object-contain'/>
+           <img src="favicon.png" className='object-contain'/>
         </div>
         <div className='md:block hidden'>
             <h1 className='text-4xl font-semibold font-[poppins]'>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React from "react";
 import {api} from '../api/api.js'
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -13,7 +13,7 @@ const Navbar = () => {
     <header className="h-[70px] w-full px-4 sticky top-0 left-0 z-10 bg-white shadow-md">
       <nav className="h-full w-full flex items-center justify-between">
         <div className="h-full w-[90px] flex justify-center items-center p-2">
-          <img src="favicon.png" className=" object-contain" />
+        <Link to="/dashboard"><img src="favicon.png" className=" object-contain" /></Link>
         </div>
         <div className="md:block hidden">
           <h1 className="text-4xl font-semibold  font-[poppins]">
